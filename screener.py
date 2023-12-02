1272c034726b9d78bd26a7cdd06b2c07c6e17b00
Karan <karanlokchandani@protonmail.com>
2023-12-02 06:13:31 +0530

    made a few workarounds

--- screener.py
+++ screener.py
@@ -1,6 +1,4 @@
 import numpy as np
-import os
-os.system('pip install yfinance')
 import yfinance as yf
 import streamlit as st
 import pandas as pd
@@ -10,7 +8,10 @@ import plotly.express as px
 st.title("Stock Screener")
 st.caption("By ~Karan Lokchandani")
 
+ticker = "MSFT"
 ticker = st.sidebar.text_input("Ticker")
+start_date = "05072005"
+end_date = "05012007"
 start_date = st.sidebar.date_input("Start Date")
 end_date = st.sidebar.date_input("End Date")
 
