eb96510cb3f19022ff22cf7c950b343d78bd03df
PhantomInTheWire <135950363+PhantomInTheWire@users.noreply.github.com>
2023-12-02 12:38:49 +0530

    Update screener.py
    fff

--- screener.py
+++ screener.py
@@ -30,7 +30,7 @@ with pricing_data:
     data2.dropna(inplace=True)
     st.write(data2)
 with fundamentals_data:
-    key = 'IW3G4PX8B3NTZQ16'
+    key = 'MXF7CUHFGALK25BE'
     fd = FundamentalData(key, output_format='pandas')
     st.subheader('Balance Sheet')
     balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
