18611a0df4a7fd1ab8fa62c892bed8b937c3f3e9
Karan <karanlokchandani@protonmail.com>
2023-12-06 14:06:03 +0530

    API CRASH FIX

--- screener.py
+++ screener.py
@@ -44,7 +44,7 @@ with pricing_data:
     st.write(data2)
 
 with fundamentals_data:
-    key = 'FBKRH19HGUCB6Z6D'
+    key = 'UAIT123S50BA4KFR'
     fd = FundamentalData(key, output_format='pandas')
     st.subheader('Balance Sheet')
     balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
